5bfe2e4cc97d3a19d6e17da8977763557bb854e8
Anushka Deshan <anushkadeshan@gmail.com>
2018-06-23 10:16:39 +0530

    Updated login and sign up page

---
+++ admin_panel/resources/views/includes/header.blade.php
@@ -0,0 +1,16 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Logo -->
+            <div class="logo" style="display:inline;">
+                <h1 style="display:inline;"><a href="{{ url('/home') }}">Bootstrap Admin Theme</a></h1>
+                <div style="display:inline;" class="top-right links">
+                    <a href="{{ route('login') }}">Login</a>
+                    <a href="{{ route('register') }}">Register</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
